ba75afcc480945ac32d1c65c8a3320ef2663fd5e
Truong Nguyen Van <truong.nguyenvan@orientsoftware.com>
2025-04-17 22:37:01 +0700

    feat: get products

--- MyApp/MyApp.Infrastructure/Seeds/Seeders.cs
+++ MyApp/MyApp.Infrastructure/Seeds/Seeders.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using MyApp.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyApp.Infrastructure.Seeds
 {
